8c0529306f2690801c9fc1baf52b89e92fde0f9f
AntiMatter418 <matthew.rose@student.csd509j.net>
2025-06-05 21:47:57 -0700

    i added melee attacks

--- main.ts
+++ main.ts
@@ -3,27 +3,36 @@ namespace SpriteKind {
     export const sword = SpriteKind.create()
     export const enemyprojectile = SpriteKind.create()
     export const parryedprojectile = SpriteKind.create()
+    export const damaging_sword = SpriteKind.create()
+    export const homing_enemy_projectile = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.parryedprojectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(sprite)
     statusbar2.value += -4
 })
-function hitstun2 () {
+function challenger3_attack () {
+	
+}
+function hitstunfunction () {
     hitstun = true
-    pause(100)
+    pause(25)
     hitstun = false
+}
+function pause2 (time: number) {
+    pause(time)
+    return true
 }
 browserEvents.onMouseMove(function (x, y) {
     mySprite2.setPosition(x + (scene.cameraProperty(CameraProperty.X) - scene.screenWidth() / 2), y + (scene.cameraProperty(CameraProperty.Y) - scene.screenHeight() / 2))
 })
-scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
-    sprites.destroy(sprite)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.enemyprojectile, function (sprite, otherSprite) {
+function challenger2_attack () {
+	
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.homing_enemy_projectile, function (sprite, otherSprite) {
     if (parrytriger == true) {
-        spriteutils.setVelocityAtAngle(otherSprite, spriteutils.angleFrom(sprite, myoponent1), 250)
+        spriteutils.setVelocityAtAngle(otherSprite, spriteutils.angleFrom(sprite, myoponent1), 200)
         otherSprite.setKind(SpriteKind.parryedprojectile)
-        hitstun()
+        hitstunfunction()
     } else if (dash == true) {
         info.changeLifeBy(0)
     } else {
@@ -31,15 +40,73 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.enemyprojectile, function (sprit
         sprites.destroy(otherSprite)
     }
 })
-let projectile2: Sprite = null
+function challenger1_attack () {
+    pause(650)
+    projectile2 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . 3 1 1 3 . . . . . . 
+        . . . . . 2 1 1 1 1 2 . . . . . 
+        . . . . . 2 1 1 1 1 2 . . . . . 
+        . . . . . . 3 1 1 3 . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, myoponent1, 0, 0)
+    projectile2.setKind(SpriteKind.enemyprojectile)
+    spriteutils.setVelocityAtAngle(projectile2, spriteutils.angleFrom(myoponent1, mySprite), 150)
+    projectile2.setFlag(SpriteFlag.AutoDestroy, false)
+}
+scene.onHitWall(SpriteKind.parryedprojectile, function (sprite, location) {
+    sprites.destroy(sprite)
+})
+sprites.onOverlap(SpriteKind.damaging_sword, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (swordstartup == true) {
+        statusbar2.value += -1.5
+        pause(100)
+    } else {
+        statusbar2.value += -0.03
+    }
+})
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    sprites.destroy(sprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.enemyprojectile, function (sprite, otherSprite) {
+    if (parrytriger == true) {
+        spriteutils.setVelocityAtAngle(otherSprite, spriteutils.angleFrom(sprite, myoponent1), 300)
+        otherSprite.setKind(SpriteKind.parryedprojectile)
+        hitstunfunction()
+    } else if (dash == true) {
+        info.changeLifeBy(0)
+    } else {
+        info.changeLifeBy(-1)
+        sprites.destroy(otherSprite)
+    }
+})
+let list: Sprite[] = []
 let swordrot = 0
+let projectile2: Sprite = null
 let statusbar2: StatusBarSprite = null
+let mySprite3: Sprite = null
 let myoponent1: Sprite = null
+let mySprite: Sprite = null
 let mySprite2: Sprite = null
 let dash = false
 let parrytriger = false
+let swordstartup = false
 let hitstun = false
+let challenger1 = true
+let challenger2 = false
+let challenger3 = false
 hitstun = false
+swordstartup = false
 parrytriger = false
 dash = false
 info.setLife(5)
@@ -93,7 +160,7 @@ let playersword = sprites.create(img`
     ............................................
     ............................................
     `, SpriteKind.sword)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -147,6 +214,27 @@ for (let value of tiles.getTilesByType(sprites.dungeon.collectibleInsignia)) {
 for (let value of tiles.getTilesByType(sprites.dungeon.collectibleRedCrystal)) {
     tiles.placeOnTile(myoponent1, value)
 }
+for (let value of tiles.getTilesByType(sprites.dungeon.collectibleBlueCrystal)) {
+    mySprite3 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 7 . . . . . . . 
+        . . . . . . 5 1 1 5 . . . . . . 
+        . . . . . 7 1 1 1 1 7 . . . . . 
+        . . . . . 7 1 1 1 1 7 . . . . . 
+        . . . . . . 5 1 1 5 . . . . . . 
+        . . . . . . . 7 7 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.homing_enemy_projectile)
+    tiles.placeOnTile(mySprite3, value)
+}
 let statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.setBarSize(2, 15)
 statusbar.setColor(6, 5)
@@ -164,17 +252,28 @@ game.onUpdate(function () {
     if (swordrot < 0) {
         swordrot += 360
     }
-    spriteutils.placeAngleFrom(
-    playersword,
-    spriteutils.degreesToRadians(swordrot),
-    11,
-    mySprite
-    )
+    if (browserEvents.MouseLeft.isPressed()) {
+        spriteutils.placeAngleFrom(
+        playersword,
+        spriteutils.degreesToRadians(swordrot),
+        randint(25, 37),
+        mySprite
+        )
+        playersword.setKind(SpriteKind.damaging_sword)
+    } else {
+        spriteutils.placeAngleFrom(
+        playersword,
+        spriteutils.degreesToRadians(swordrot),
+        11,
+        mySprite
+        )
+        playersword.setKind(SpriteKind.sword)
+    }
     playersword.setImage(sword_rotations[Math.round(swordrot)])
 })
 game.onUpdate(function () {
     if (hitstun == true) {
-        pause(200)
+        pause(25)
     }
 })
 game.onUpdateInterval(2000, function () {
@@ -182,28 +281,12 @@ game.onUpdateInterval(2000, function () {
         statusbar.value += 1
     }
 })
-game.onUpdateInterval(4000, function () {
-    projectile2 = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . 3 1 1 3 . . . . . . 
-        . . . . . 2 1 1 1 1 2 . . . . . 
-        . . . . . 2 1 1 1 1 2 . . . . . 
-        . . . . . . 3 1 1 3 . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, myoponent1, 0, 0)
-    projectile2.setKind(SpriteKind.enemyprojectile)
-    spriteutils.setVelocityAtAngle(projectile2, spriteutils.angleFrom(myoponent1, mySprite), 150)
-    projectile2.setFlag(SpriteFlag.AutoDestroy, false)
+forever(function () {
+    pauseUntil(() => browserEvents.MouseLeft.isPressed())
+    swordstartup = true
+    pause(100)
+    swordstartup = false
+    pauseUntil(() => !(browserEvents.MouseLeft.isPressed()))
 })
 forever(function () {
     pauseUntil(() => controller.A.isPressed())
@@ -246,7 +329,7 @@ forever(function () {
         . . . f f f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
         `)
-    pause(200)
+    pauseUntil(() => hitstun || pause2(200))
 })
 forever(function () {
     pauseUntil(() => browserEvents.Shift.isPressed())
@@ -258,7 +341,25 @@ forever(function () {
         pause(100)
         mySprite.setVelocity(0, 0)
         controller.moveSprite(mySprite, 150, 150)
-        pause(200)
         dash = false
+        pause(100)
+    }
+})
+forever(function () {
+    if (challenger1 == true) {
+        challenger1_attack()
+    } else if (challenger2 == true) {
+        challenger2_attack()
+    } else if (challenger3 == true) {
+        challenger3_attack()
+    }
+})
+game.onUpdateInterval(500, function () {
+    for (let value of sprites.allOfKind(SpriteKind.homing_enemy_projectile)) {
+        list = spriteutils.getSpritesWithin(SpriteKind.Player, 100, value)
+        list.reverse()
+        for (let value2 of list) {
+            spriteutils.setVelocityAtAngle(value, spriteutils.angleFrom(value, value2), 100)
+        }
     }
 })
